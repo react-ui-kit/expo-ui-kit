07c2b11dfb1164c2c4b7b6714af2140832e1dc95
Hetmann W. Iohan <hetmann@users.noreply.github.com>
2019-05-26 13:05:27 +0300

    Dev (#3)
    * DEV: updated README with Expo info
    
    * 0.0.2
    
    * Added homepage + keywords
    
    * DEV: added components, utils, default theme, NO tests

---
+++ src/theme.js
@@ -0,0 +1,46 @@
+const COLORS = {
+  // base colors
+  primary: "#4630EB",
+  secondary: "#A3A1F7",
+  tertiary: "#FFE358",
+
+  // non-colors
+  black: "#000020",
+  white: "#FFFFFF",
+
+  // color variations
+  gray: "#535453",
+  error: "#DC3545",
+  warning: "#FFE358",
+  success: "#4CD964",
+  info: "#4DA1FF"
+};
+
+const SIZES = {
+  // global sizes
+  base: 8,
+  font: 16,
+  radius: 4,
+  padding: 24,
+
+  // font sizes
+  h1: 34,
+  h2: 24,
+  h3: 20,
+  title: 18,
+  subtitle: 14,
+  caption: 12,
+  small: 10
+};
+
+const FONTS = {
+  h1: { fontSize: SIZES.h1, letterSpacing: 0.15 },
+  h2: { fontSize: SIZES.h2, letterSpacing: 0 },
+  h3: { fontSize: SIZES.h3, letterSpacing: 0.15 },
+  title: { fontSize: SIZES.title, letterSpacing: 0.15 },
+  subtitle: { fontSize: SIZES.body },
+  caption: { fontSize: SIZES.caption, letterSpacing: 0.4 },
+  small: { fontSize: SIZES.small, letterSpacing: 1.5 }
+};
+
+export { COLORS, SIZES, FONTS };
