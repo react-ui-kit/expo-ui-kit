d4faaaebe73915d7587e45f57584c22d1bca7ffe
Hetmann Wilhelm Iohan <contact@blogtools.ro>
2019-09-11 17:31:28 +0300

    DEV: added readme/example for Card component

--- src/Card.js
+++ src/Card.js
@@ -6,6 +6,50 @@ import expoTheme from "./theme";
 
 import Block from "./Block";
 
+/**
+ * https://facebook.github.io/react-native/docs/view
+ * https://facebook.github.io/react-native/docs/flexbox
+ *
+ * Using Block component with predefined props: color, radius and padding
+ * <Card>
+ *  <Text>default card</Text>
+ * </Card>
+ *
+ * Border radius using radius props
+ *
+ * <Card radius={8}>
+ *  <Text>radius={8}</Text>
+ * </Card>
+ *
+ * Padding using padding props
+ * default padding={SIZES.base}
+ *
+ * <Card padding={12}>
+ *  <Text>padding={12}</Text>
+ * </Card>
+ *
+ * Set shadow using shadow props
+ * default shadow with color black and elevation
+ * shadowOffset is calculated using elevation - 1
+ * shadowRadius is equal with elevation value
+ *
+ * <Card shadow>
+ *  <Text>shadow</Text>
+ * </Card>
+ *
+ * <Card shadow elevation={2}>
+ *  <Text>shadow elevation={2}</Text>
+ * </Card>
+ *
+ * Set borderColor using outlined prop
+ * default borderWidth: 1 and borderColor: rgba(COLORS.black, 0.16) with alpha 0.16
+ *
+ * <Card outlined>
+ *  <Text>outlined</Text>
+ * </Card>
+ *
+ */
+
 class Card extends Component {
   render() {
     const {
