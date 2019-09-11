0774a9135ecea559bb812a213a6f0510e8f5ef70
Hetmann Wilhelm Iohan <contact@blogtools.ro>
2019-09-11 16:29:31 +0300

    DEV: added readme/example for Button component

--- src/Button.js
+++ src/Button.js
@@ -4,6 +4,39 @@ import { StyleSheet, TouchableOpacity } from "react-native";
 import expoTheme from "./theme";
 import { getMargins, getPaddings, rgba, mergeTheme } from "./utils";
 
+/**
+ * https://facebook.github.io/react-native/docs/touchableopacity
+ *
+ * Colors
+ * - will render backgroundColor using predefined colors from theme.js COLORS array
+ * - predefined colors: primary, secondary, tertiary, black, white, gray, error, warning, success, info
+ *
+ * <Button primary><Text>backgroundColor: COLORS.primary</Text></Button>
+ * <Button transparent><Text>backgroundColor: "transparent"</Text></Button>
+ *
+ * - custom color using hex color
+ * <Button color="#DDDDDD"><Text>backgroundColor: #DDDDDD</Text></Button>
+ *
+ * Set activeOpacity using opacity prop
+ * default activeOpacity=0.8
+ * <Button opacity={0.5}><Text>opacity={0.5}</Text></Button>
+ *
+ * Outlined and add borderColor equal with backgroundColor
+ * <Button primary outlined><Text>outlined</Text></Button>
+ *
+ * Disabling the button
+ * <Button disabled><Text>disabled</Text></Button>
+ * <Button disabled={false}><Text>false</Text></Button>
+ *
+ * Add flex to button style
+ * <Button flex><Text>flex=1</Text></Button>
+ * <Button flex={2}><Text>flex=2</Text></Button>
+ *
+ * Add height to button style
+ * <Button height={58}><Text>height=58</Text></Button>
+ *
+ */
+
 class Button extends Component {
   render() {
     const {
