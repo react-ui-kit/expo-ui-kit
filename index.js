0c61037d6278934a2da7a325f1985907f25a968f
Hetmann W. Iohan <contact@react-ui-kit.com>
2020-02-28 17:34:40 +0200

    DEV: export components (#31)
    * DEV: updated README with Expo info
    
    * 0.0.2
    
    * Added homepage + keywords
    
    * DEV: added components, utils, default theme, NO tests
    
    * DEV: added tests for Input & Text, updates for components: Block, Input, Text
    
    * DEV: added tests for Input & Text, updates for components: Block, Input, Text
    
    * DEV: Button - borderRadius fix & Input borderColor using color prop, default height to 44pt
    
    * DEV: fix margin & padding utils, added margin & padding to Text, small updates on Block
    
    * DEV: added type prop on input for: email, phone number & custom
    
    * 0.0.5
    
    * DEV: added type prop on input for: email, phone number & custom
    
    * DEV: update to expo 33.0.0, react 16.8.3, react-native 0.59.8
    
    * DEV: renamed internal onFocus, onBlur and onChange to handleFocus, handleBlur and handleChangeText | updated tests for Input component
    
    * DEV: added internalRef for ref
    
    * DEV: deleted flex from defaultProps, version to 0.0.5
    
    * DEV: upgrade to Expo SDK 34.0.1
    
    * DEV: added SafeAreaView using safe props
    
    * DEV: export theme from Utils, added width & height from Dimensions
    
    * DEV: added Animated.Text to Typography component
    
    * DEV: ignore coverage
    
    * 0.0.7
    
    * DEV: fix for #13
    
    * DEV: margin & padding improvement for Text component
    
    * DEV: fix for #10 Block component
    
    * DEV: fix #8, #9, #15, #16
    
    * DEV: PureComponent to Component
    
    * DEV: added shadow / elevation on Card component
    
    * DEV: added usage for Text component on task #18, changed Block to use flex: 0 by default instead of flex: 1
    
    * DEV: Block to use flex: 0 by default instead of flex: 1
    
    * DEV: Block to use flex: 0 by default instead of flex: 1
    
    * DEV: changed reverted to use Block flex:1 by default, tests updated to check animated & safe
    
    * DEV: partial fix for #17
    
    * DEV: fix for #17 Block component
    
    * DEV: fix for #17 Block component
    
    * DEV: fix for #17 Text component
    
    * DEV: added usage for helpers & rgab on #18
    
    * DEV: added readme/example for Block component
    
    * DEV: added readme/example for Button component
    
    * DEV: added readme/example for Button component
    
    * DEV: added readme/example for Block component
    
    * DEV: added readme/example for Card component
    
    * DEV: added support for different Touchables, fix #11
    
    * DEV: added usage for Button types
    
    * DEV: updated Button component to support spacing helper
    
    * DEV: fix Text missing Component import
    
    * DEV: button center only for vertical position #9
    
    * DEV: button center only for vertical position #9
    
    * DEV: upgrade to Expo SDK 35 and devDependencies
    
    * DEV: added parseSpacing to be used for each spacing type: margin & padding
    
    * DEV: excluded margin & padding props from rest of props
    
    * DEV: added spacing string for Text component
    
    * DEV: added spacing string for Button component
    
    * DEV: fix extraProps, replaced with reduce
    
    * DEV: fix Input methods for onChangeText, onFocus, onBlur, added arrow function for handle this events
    
    * Default font color and allow <Block marginVertical> (marginHorizontal, margin Top...). (#25)
    
    * Dev updated from master (#28)
    
    * DEV: packages updated to latest version, fix test for Text component
    
    * DEV: export components by name
    
    Co-authored-by: Jay Salvat <jay@jaysalvat.com>

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as Components from "./src";
+import { Block, Card, Button, Text, Input } from "./src";
 import * as Utils from "./src/utils";
 import * as theme from "./src/theme";
 
-module.exports = { ...Components, Utils, theme };
+module.exports = { Block, Card, Button, Text, Input, Utils, theme };
