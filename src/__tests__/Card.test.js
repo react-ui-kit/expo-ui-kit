dca16dd145c2da2dee2206a3251d3686bfbec181
Tales Pereira <42183799+taleco22@users.noreply.github.com>
2020-04-03 03:18:36 -0300

    Hooks/card (#36)
    * card to hooks
    
    * hotfix: remove redundant code

--- src/__tests__/Card.test.js
+++ src/__tests__/Card.test.js
@@ -1,28 +1,34 @@
 import React from "react";
-import renderer from "react-test-renderer";
 import { StyleSheet } from "react-native";
-
+import renderer from "react-test-renderer";
 import Card from "../Card";
 import { rgba } from "../utils";
 
 describe("<Card />", () => {
   it("render default", () => {
-    const component = renderer.create(<Card />).toJSON();
+    const card = renderer.create(<Card />);
+    const component = card.toJSON();
     const style = StyleSheet.flatten(component.props.style);
     expect(style.flex).toEqual(1);
   });
 
   it("shadow", () => {
-    const component = renderer.create(<Card shadow />).toJSON();
-    const instance = renderer.create(<Card shadow />).getInstance();
+    const card = renderer.create(<Card shadow />);
+
+    const component = card.toJSON();
+    const instance = card.root;
+
     const style = StyleSheet.flatten(component.props.style);
     expect(instance.props.shadow).toEqual(true);
     expect(style.shadowColor).toEqual("#000020");
   });
 
   it("outlined", () => {
-    const component = renderer.create(<Card outlined />).toJSON();
-    const instance = renderer.create(<Card outlined />).getInstance();
+    const card = renderer.create(<Card outlined />);
+
+    const component = card.toJSON();
+    const instance = card.root;
+
     const style = StyleSheet.flatten(component.props.style);
     expect(instance.props.outlined).toEqual(true);
     expect(style.borderColor).toEqual(rgba("#000020", 0.16));
