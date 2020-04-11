a34fa408393f16b0bcff551ce54d8e0f8cd8118a
Tales Pereira <talesdsp@gmail.com>
2020-04-11 05:13:14 -0300

    remove js files

--- index.js
+++
@@ -1,5 +0,0 @@
-import { Block, Card, Button, Text, Input } from "./src";
-import * as Utils from "./src/utils";
-import * as theme from "./src/theme";
-
-module.exports = { Block, Card, Button, Text, Input, Utils, theme };
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import Block from "./Block";
-import Card from "./Card";
-import Button from "./Button";
-import Text from "./Text";
-import Input from "./Input";
-
-export { Block, Card, Button, Text, Input };
