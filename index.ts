aec3730c2da1f83b765d7c953bfa04bb45efa958
Tales Pereira <talesdsp@gmail.com>
2020-04-22 03:08:35 -0300

    delete files

--- index.ts
+++
@@ -1,5 +0,0 @@
-import { Block, Button, Card, Input, Text } from "./src";
-import * as theme from "./src/theme";
-import * as Utils from "./src/utils";
-
-export { Block, Card, Button, Text, Input, Utils, theme };
