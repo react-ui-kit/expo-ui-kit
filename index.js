826ef99fb666ced51054f2ec128e57c157b9d489
Hetmann W. Iohan <contact@react-ui-kit.com>
2020-04-22 09:19:46 +0300

    Update index.js (#48)

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import { Block, Card, Button, Text, Input } from "./src";
 import * as Utils from "./src/utils";
 import * as theme from "./src/theme";
 
-module.exports = { Block, Card, Button, Text, Input, Utils, theme };
+export { Block, Card, Button, Text, Input, Utils, theme };
