17422ed28d7f9f2f0a9f5a67d24a16e65508421b
Hetmann W. Iohan <contact@react-ui-kit.com>
2020-05-06 08:46:07 +0300

    Dev (#62)
    * DEV: extracted usage from component comments to COMPONENTS.md file
    
    * Fix for #51
    
    * added npmignore file
    
    * DEV: refactored helpers, updates for Block, Button & Text, tests
    
    * DEV: fix Button flex issue on Android
    
    * DEV: color removed from Input due to Android bug
    
    * DEV: version 0.1.4
    
    * Hotfix (#58)
    
    * Dev (#57)
    
    * DEV: extracted usage from component comments to COMPONENTS.md file
    
    * Fix for #51
    
    * added npmignore file
    
    * DEV: refactored helpers, updates for Block, Button & Text, tests
    
    * DEV: fix Button flex issue on Android
    
    * DEV: color removed from Input due to Android bug
    
    * DEV: version 0.1.4
    
    * Fix typings
    
    * update tests
    
    Co-authored-by: Hetmann W. Iohan <contact@react-ui-kit.com>
    
    Co-authored-by: Tales Pereira <42183799+talesdsp@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Block from "./Block";
-import Button from "./Button";
+import Button, { ButtonInstance } from "./Button";
 import Card from "./Card";
 import Input from "./Input";
 import Text from "./Text";
 
-export { Block, Card, Button, Text, Input };
+export { Block, Card, Button, ButtonInstance, Text, Input };
